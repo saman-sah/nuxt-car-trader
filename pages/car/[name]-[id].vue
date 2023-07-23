274f2e3238abb7b986c9018ef563124220e2915f
SamanSah <sahraei.saman91@gmail.com>
2023-07-23 19:04:47 +0330

    initial pages and component

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- CAR DETAIL PAGE -->
+    <div
+    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+    >
+        <div>
+            <!-- CAR HERO -->
+            <DetailHero />
+            <!-- CAR HERO -->
+        </div>
+
+        <!-- CAR ATTRIBUTES -->
+        <DetailAttributes />
+        <!-- CAR ATTRIBUTES -->
+
+        <!-- CAR DESCRISPTION -->
+        <DetailDescription />
+        <!-- CAR DESCRISPTION -->
+
+        <!-- CAR CONTACT -->
+        <DetailContact />
+        <!-- CAR CONTACT -->
+
+    </div>
+    <!-- CAR DETAIL PAGE   -->
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <!-- NAVBAR -->
+        <Nav />
+        <!-- NAVBAR -->
+
+        <!-- HOME HERO -->
+        <CarHero />
+        <!-- HOME HERO -->
+    </div>
+</template>
+
+
+<style>
+
+</style>
